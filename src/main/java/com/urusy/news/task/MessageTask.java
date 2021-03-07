e6d09d949b8f44658afe7b8bd89093be9a07a8a8
urushima <urushima0613@gmail.com>
2021-03-07 15:29:37 +0900

    Add Schedule run

--- src/main/java/com/urusy/news/task/MessageTask.java
+++ src/main/java/com/urusy/news/task/MessageTask.java
@@ -1,21 +1,22 @@
 package com.urusy.news.task;
 
-import com.urusy.news.messaging.PrintMessagingGateway;
+import com.urusy.news.service.HackerNewsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-
-//@Component
+@Slf4j
 @RequiredArgsConstructor
+@Component
 public class MessageTask {
 
-    private final PrintMessagingGateway printMessagingGateway;
+    private final HackerNewsServiceImpl hackerNewsService;
 
-    @Scheduled(cron = "${schedule.message}")
-    public void pushMessage() {
-        printMessagingGateway.send("Scheduled message! " + LocalDateTime.now());
-        System.out.println("Message send.");
+    @Scheduled(cron = "${schedule.hacker-news}")
+    public void getHackerNews() {
+        log.info("Start getHackerNews");
+        hackerNewsService.getBestStories();
+        log.info("Finish getHackerNews");
     }
 }
